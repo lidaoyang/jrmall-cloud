ab86c4d37ab039813691e00f781b5be6537e8a22
daoyang <lidaoyang@hotmail.com>
2025-06-19 16:35:30 +0800

    perf(gateway): 优化限流降级提示信息
    -增加了对不同类型的 Sentinel异常的处理
    - 提供了更具体的错误提示信息，包括限流、降级、参数限流、系统限制和授权失败等情况
    - 保留了原有的 ResultCode.FLOW_LIMITING 返回格式

--- pilates-gateway/src/main/java/com/jrmall/pilates/gateway/config/SentinelConfig.java
+++ pilates-gateway/src/main/java/com/jrmall/pilates/gateway/config/SentinelConfig.java
@@ -1,6 +1,11 @@
 package com.jrmall.pilates.gateway.config;
 
 import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
+import com.alibaba.csp.sentinel.slots.block.authority.AuthorityException;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeException;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowException;
+import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowException;
+import com.alibaba.csp.sentinel.slots.system.SystemBlockException;
 import com.jrmall.pilates.common.result.ResultCode;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
@@ -21,13 +26,24 @@ public class SentinelConfig {
     @PostConstruct
     private void initBlockHandler() {
         GatewayCallbackManager.setBlockHandler(
-                (exchange, t) ->
-                        ServerResponse
-                                .status(HttpStatus.TOO_MANY_REQUESTS)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .body(
-                                        BodyInserters.fromValue(ResultCode.FLOW_LIMITING.toString())
-                                )
+                (exchange, ex) -> {
+                    String msg = "触发服务保护！";
+                    if (ex instanceof FlowException) {
+                        msg = "请求过于频繁，被限流了！";
+                    } else if (ex instanceof DegradeException) {
+                        msg = "请求过于频繁，被降级了！";
+                    } else if (ex instanceof ParamFlowException) {
+                        msg = "热点数据请求过于频繁！";
+                    } else if (ex instanceof SystemBlockException) {
+                        msg = "请求过于频繁，触发系统限制！";
+                    } else if (ex instanceof AuthorityException) {
+                        msg = "授权规则不通过！";
+                    }
+                    return ServerResponse
+                            .status(HttpStatus.TOO_MANY_REQUESTS)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .body(BodyInserters.fromValue(ResultCode.FLOW_LIMITING.toString()));
+                }
         );
     }
 }
